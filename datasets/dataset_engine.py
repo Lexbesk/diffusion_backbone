15e8a55eb0c049488415bf839e38b6bc09d25f44
nickgkan <nickganats@yahoo.gr>
2024-10-22 09:45:57 -0400

    Check for numpy or torch in dataset class

--- datasets/dataset_engine.py
+++ datasets/dataset_engine.py
@@ -200,7 +200,11 @@ class RLBenchDataset(Dataset):
             )
 
         # Get action tensors for respective frame ids
-        action = torch.cat([torch.from_numpy(episode[2][i]) for i in frame_ids])
+        action = torch.cat([
+            episode[2][i] if isinstance(episode[2][i], torch.Tensor)
+            else torch.from_numpy(episode[2][i])
+            for i in frame_ids
+        ])
 
         # Sample one instruction feature
         if self._instructions:
@@ -210,12 +214,24 @@ class RLBenchDataset(Dataset):
             instr = torch.zeros((rgbs.shape[0], 53, 512))
 
         # Get gripper tensors for respective frame ids
-        gripper = torch.cat([torch.from_numpy(episode[4][i]) for i in frame_ids])
+        gripper = torch.cat([
+            episode[4][i] if isinstance(episode[4][i], torch.Tensor)
+            else torch.from_numpy(episode[4][i])
+            for i in frame_ids
+        ])
 
         # gripper history
         gripper_history = torch.stack([
-            torch.cat([torch.from_numpy(episode[4][max(0, i-2)]) for i in frame_ids]),
-            torch.cat([torch.from_numpy(episode[4][max(0, i-1)]) for i in frame_ids]),
+            torch.cat([
+                episode[4][max(0, i-2)] if isinstance(episode[4][i], torch.Tensor)
+                else torch.from_numpy(episode[4][max(0, i-2)])
+                for i in frame_ids
+            ]),
+            torch.cat([
+                episode[4][max(0, i-1)] if isinstance(episode[4][i], torch.Tensor)
+                else torch.from_numpy(episode[4][max(0, i-1)])
+                for i in frame_ids
+            ]),
             gripper
         ], dim=1)
 
