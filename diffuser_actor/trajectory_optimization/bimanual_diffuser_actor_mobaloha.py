e77df520e0cf264a4ee081ab76cd5498b02c6739
Jiahe Xu <xjh4438318846@gmail.com>
2024-10-28 16:16:52 -0400

    remove seed

--- diffuser_actor/trajectory_optimization/bimanual_diffuser_actor_mobaloha.py
+++ diffuser_actor/trajectory_optimization/bimanual_diffuser_actor_mobaloha.py
@@ -132,8 +132,9 @@ class BiManualDiffuserActor(DiffuserActor):
 
         # Sample
         # <need to remove>
-        torch.manual_seed(0)
-        np.random.seed(0)
+        # torch.manual_seed(0)
+        # np.random.seed(0)
+        
         trajectory = self.conditional_sample(
             cond_data,
             cond_mask,
