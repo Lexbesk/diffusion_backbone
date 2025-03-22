e541d7ee3bf6b8eff5805f9e3ca215ce7acf4400
nickgkan <nickganats@yahoo.gr>
2025-03-22 22:45:07 +0800

    Cleaned scripts

--- scripts/rlbench/train_peract2_fast.sh
+++ scripts/rlbench/train_peract2_fast.sh
@@ -12,23 +12,20 @@ denoise_timesteps=10  # 10
 denoise_model=rectified_flow
 keypose_only=true
 quaternion_format=xyzw
-rotation_parametrization=6D
 fps_subsampling_factor=5
 backbone=clip
-use_instruction=true
+bimanual=true
 workspace_normalizer_buffer=0.05
 B=64
 B_val=64
 C=120
 num_attn_heads=8
-num_vis_ins_attn_layers=3
+num_vis_instr_attn_layers=3
 train_iters=600000
 val_freq=4000
-precompute_instruction_encodings=false
 num_workers=4
 dataset=Peract2
 ngpus=4
-bimanual=True
 
 run_log_dir=newC$C-B$B-lr$lr-$lr_scheduler-H$num_history-$denoise_model-DT$denoise_timesteps
 checkpoint=train_logs/${main_dir}/${run_log_dir}/last.pth
@@ -36,21 +33,19 @@ checkpoint=train_logs/${main_dir}/${run_log_dir}/last.pth
 eval_only=false
 
 torchrun --nproc_per_node $ngpus --master_port $RANDOM \
-    main_fast_new.py \
+    main.py \
     --dataset $dataset \
     --train_data_dir $train_data_dir \
     --eval_data_dir $eval_data_dir \
     --train_instructions $train_instructions \
     --val_instructions $val_instructions \
-    --precompute_instruction_encodings $precompute_instruction_encodings \
     --workspace_normalizer_buffer $workspace_normalizer_buffer \
     --num_workers $num_workers \
     --train_iters $train_iters \
+    --bimanual $bimanual \
     --embedding_dim $C \
     --num_attn_heads $num_attn_heads \
-    --num_vis_ins_attn_layers $num_vis_ins_attn_layers \
-    --use_instruction $use_instruction \
-    --rotation_parametrization $rotation_parametrization \
+    --num_vis_instr_attn_layers $num_vis_instr_attn_layers \
     --fps_subsampling_factor $fps_subsampling_factor \
     --backbone $backbone \
     --quaternion_format $quaternion_format \
@@ -66,5 +61,4 @@ torchrun --nproc_per_node $ngpus --master_port $RANDOM \
     --eval_only $eval_only \
     --checkpoint $checkpoint \
     --exp_log_dir $main_dir \
-    --run_log_dir ${run_log_dir} \
-    --bimanual $bimanual
+    --run_log_dir ${run_log_dir}
