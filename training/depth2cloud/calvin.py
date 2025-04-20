f2b46166fb81a9f46c8c6e9e78d7a32d37af3da5
Nikolaos Gkanatsios <mackintoshtoast@gmailc.om>
2025-04-20 13:15:12 -0400

    Optimized depth2cloud

--- training/depth2cloud/calvin.py
+++ training/depth2cloud/calvin.py
@@ -4,40 +4,82 @@ import torch
 
 class CALVINDepth2Cloud:
 
-    def __init__(self):
+    def __init__(self, front_shape=(160, 160)):
+        self.uvs = {}
+        # Static camera (front)
         self.T_world_cam = torch.from_numpy(np.linalg.inv(np.asarray([
             0.7232445478439331, -0.030260592699050903, 0.6899287700653076, 0.0,
             0.5141233801841736, 0.6906105875968933, -0.5086592435836792, 0.0,
             -0.46107974648475647, 0.7225935459136963, 0.5150379538536072, 0.0,
             0.21526622772216797, -0.26317155361175537, -4.399168968200684, 1.0
-        ]).reshape((4, 4)).T)).float().cuda()
-        self.width, self.height, self.fov = 200, 200, 10
-        self.foc = self.height / (2 * np.tan(np.deg2rad(self.fov) / 2))
+        ]).reshape((4, 4)).T)).float().cuda(non_blocking=True)
+        # Front camera: (height, width, fov) = (200, 200, 10)
+        self.uvs['front'] = self._get_uv(
+            200, 200,
+            front_shape,  # assume center crop if different than (200, 200)
+            200 / (2 * np.tan(np.deg2rad(10) / 2))  # h / (2 * tan(fov / 2))
+        )
 
-    def __call__(self, depth):
+        # Gripper camera (wrist): (height, width, fov) = (84, 84, 75)
+        self.uvs['wrist'] = self._get_uv(
+            84, 84,
+            (84, 84),
+            84 / (2 * np.tan(np.deg2rad(75) / 2))
+        )
+
+    def _get_uv(self, h, w, img_size, foc):
+        v, u = torch.meshgrid(torch.arange(h), torch.arange(w), indexing='ij')
+        # Center crop
+        h_offset = (h - img_size[0]) // 2
+        w_offset = (w - img_size[1]) // 2
+        v = v[h_offset:img_size[0]+h_offset, w_offset:img_size[1]+w_offset]
+        u = u[h_offset:img_size[0]+h_offset, w_offset:img_size[1]+w_offset]
+        # Move to device
+        u, v = u.cuda(non_blocking=True), v.cuda(non_blocking=True)
+        # Calculate 'instrinsics' matrix
+        uv_mat = torch.stack((
+            (u  - w // 2) / foc,
+            (-v + h // 2) / foc,
+            -torch.ones_like(u)
+        ), dim=0)  # (3, H, W)
+        return uv_mat.reshape(3, -1)  # (3, H*W)
+
+    def deproject(self, depth, extrinsics, cam):
         """
         Args
-            depth: (B, H, W), if H,W != 200, we assume center crop
+            depth: (B, H, W)
+            extrinsics: (B, 4, 4)
+            cam: str, 'front' or 'wrist'
+
         Output
             point cloud: (B, 3, H, W)
         """
         b, h, w = depth.shape
-        v, u = torch.meshgrid(torch.arange(200), torch.arange(200))
-        h_offset = (200 - h) // 2
-        w_offset = (200 - w) // 2
-        v = v[h_offset:h+h_offset, w_offset:w+w_offset].to(depth.device)
-        u = u[h_offset:h+h_offset, w_offset:w+w_offset].to(depth.device)
 
         # Camera XYZ
-        x = (u - self.width // 2)[None] * depth / self.foc
-        y = -(v - self.height // 2)[None] * depth / self.foc
-        z = -depth
-        ones = torch.ones_like(z)
+        cam_pos = self.uvs[cam][None] * depth.reshape(b, 1, -1)  # B 3 HW
 
-        # Unproject to world coordinates
-        cam_pos = torch.stack([x, y, z, ones], 1).reshape(b, 4, -1)  # B 4 HW
-        T_world_cam = self.T_world_cam[None].repeat(b, 1, 1)  # B 4 4
-        world_pos = torch.matmul(T_world_cam, cam_pos)  # B 4 HW
+        # # Unproject to world coordinates
+        cam_pos = torch.cat((cam_pos, torch.ones_like(cam_pos[:, :1])), dim=1)
+        world_pos = torch.bmm(extrinsics[:, :3], cam_pos)  # B 3 HW
 
         # Reshape
-        return world_pos[:, :3].reshape(b, 3, h, w)
+        return world_pos.reshape(b, 3, h, w)
+
+    def __call__(self, depth_front, depth_wrist, extrinsics_wrist):
+        pcd_front = None
+        if depth_front is not None:
+            pcd_front = self.deproject(
+                depth_front,
+                self.T_world_cam[None].repeat(len(depth_front), 1, 1),
+                'front'
+            )
+            self.T_world_cam[None].repeat(len(depth_front), 1, 1)  # B 4 4
+        pcd_wrist = None
+        if depth_wrist is not None:
+            pcd_wrist = self.deproject(
+                depth_wrist,
+                extrinsics_wrist,
+                'wrist'
+            )
+        return pcd_front, pcd_wrist
--- training/depth2cloud/rlbench.py
+++ training/depth2cloud/rlbench.py
@@ -7,53 +7,48 @@ class RLBenchDepth2Cloud:
     def __init__(self, shape):
         self.uniforms = torch.from_numpy(
             self._create_uniform_pixel_coords_image(shape)
-        ).permute(2, 0, 1)  # (3, H, W)
+        ).permute(2, 0, 1).cuda(non_blocking=True)  # (3, H, W)
 
     @staticmethod
     def _create_uniform_pixel_coords_image(resolution):
-        pixel_x_coords = np.reshape(
-            np.tile(np.arange(resolution[1]), [resolution[0]]),
-            (resolution[0], resolution[1], 1)).astype(np.uint8)
-        pixel_y_coords = np.reshape(
-            np.tile(np.arange(resolution[0]), [resolution[1]]),
-            (resolution[1], resolution[0], 1)).astype(np.uint8)
-        pixel_y_coords = np.transpose(pixel_y_coords, (1, 0, 2))
-        uniform_pixel_coords = np.concatenate(
-            (pixel_x_coords, pixel_y_coords, np.ones_like(pixel_x_coords)), -1)
-        return uniform_pixel_coords
+        h, w = resolution
+        u = np.tile(np.arange(w), (h, 1))
+        v = np.tile(np.arange(h), (w, 1)).T
+        ones = np.ones_like(u)
+        return np.stack((u, v, ones), axis=-1)  # (H, W, 3)
 
     @staticmethod
     def _get_cam_proj_mat_inv_b(extrinsics, intrinsics):
-        # Construct camera projection
-        C = extrinsics[:, :3, [3]]  # (B, 3, 1)
+        # extrinsics: (B, 4, 4), intrinsics: (B, 3, 3)
+        C = extrinsics[:, :3, 3:]  # (B, 3, 1)
         R = extrinsics[:, :3, :3]  # (B, 3, 3)
-        R_inv = R.permute(0, 2, 1)  # inverse of rot matrix is transpose
-        R_inv_C = torch.matmul(R_inv, C)  # (B, 3, 1)
-        extrinsics = torch.cat((R_inv, -R_inv_C), -1)  # (B, 3, 4)
-        cam_proj_mat = torch.matmul(intrinsics, extrinsics)  # (B, 3, 4)
-        cam_proj_mat_homo = torch.cat([
-            cam_proj_mat,
+        R_inv = R.transpose(1, 2)  # (B, 3, 3)
+        t_inv = -torch.bmm(R_inv, C)  # (B, 3, 1)
+        ext_inv = torch.cat([R_inv, t_inv], dim=-1)  # (B, 3, 4)
+        cam_proj = torch.bmm(intrinsics, ext_inv)  # (B, 3, 4)
+        cam_proj_homo = torch.cat([
+            cam_proj,
             torch.tensor(
                 [0, 0, 0, 1],
-                dtype=cam_proj_mat.dtype,
-                device=cam_proj_mat.device
-            )[None, None].repeat(len(cam_proj_mat), 1, 1)
-        ], 1)  # (B, 4, 4)
-        cam_proj_mat_inv = torch.linalg.inv(cam_proj_mat_homo)[:, :3]
-        return cam_proj_mat_inv  # (B, 3, 4)
+                dtype=cam_proj.dtype,
+                device=cam_proj.device
+            )[None, None].expand(cam_proj.size(0), 1, 4)
+        ], dim=1)  # (B, 4, 4)
+        cam_proj_inv = torch.linalg.inv(cam_proj_homo.float())[:, :3]
+        return cam_proj_inv.to(cam_proj_homo.dtype)  # (B, 3, 4)
 
     def unproject(self, depth, extrinsics, intrinsics):
         # depth is (B, H, W), extrinsics (B, 4, 4), intrinsics (B, 3, 3)
         # output is (B, 3, H, W)
-        device = depth.device
-        pc = self.uniforms[None].to(device) * depth[:, None]  # (B, 3, H, W)
-        b, _, h, w = pc.shape
-        pc = torch.cat([pc, torch.ones_like(pc[:, :1])], 1)  # (B, 4, H, W)
-        pc = pc.view(b, 4, -1)  # (4, B*H*W)
-        cam_proj_mat_inv = self._get_cam_proj_mat_inv_b(extrinsics, intrinsics)
-        pc = torch.matmul(cam_proj_mat_inv, pc)  # (B, 3, H*W)
-        pc = pc.reshape(b, 3, h, w)  # (B, 3, H, W)
-        return pc
+        b, h, w = depth.shape
+        uv1 = self.uniforms[None]  # (1, 3, H, W)
+        pc = uv1 * depth[:, None]  # (B, 3, H, W)
+        pc = torch.cat([pc, torch.ones_like(pc[:, :1])], dim=1)  # (B, 4, H, W)
+        pc = pc.reshape(b, 4, -1)  # (B, 4, HW)
+
+        cam_proj_inv = self._get_cam_proj_mat_inv_b(extrinsics, intrinsics)
+        world_pc = torch.bmm(cam_proj_inv, pc)  # (B, 3, HW)
+        return world_pc.reshape(b, 3, h, w)
 
     def __call__(self, depth, extrinsics, intrinsics):
         # depth is (B Nc H W), extrinsics (B Nc 4 4), intrinsics (B Nc 3 3)
@@ -61,8 +56,8 @@ class RLBenchDepth2Cloud:
         # Nc is the number of cameras
         b, nc, h, w = depth.shape
         pc = self.unproject(
-            depth.flatten(0, 1),
-            extrinsics.flatten(0, 1),
-            intrinsics.flatten(0, 1)
+            depth.view(-1, h, w),
+            extrinsics.view(-1, 4, 4),
+            intrinsics.view(-1, 3, 3)
         )
         return pc.reshape(b, nc, 3, h, w)
