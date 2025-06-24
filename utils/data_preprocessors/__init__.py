07d539798b442293b1ab3015fd04fa18b0146bb5
Nikolaos Gkanatsios <mackintoshtoast@gmailc.om>
2025-06-24 14:04:46 -0400

    Changes so that peract becomes a separate rlbench case

--- utils/data_preprocessors/__init__.py
+++ utils/data_preprocessors/__init__.py
@@ -1,6 +1,7 @@
 from functools import partial
 
 from .calvin import CALVINDataPreprocessor
+from .peract import PeractDataPreprocessor
 from .rlbench import RLBenchDataPreprocessor
 
 
@@ -8,7 +9,9 @@ def fetch_data_preprocessor(dataset_name):
     dataset_name = dataset_name.lower()
     if 'peract2' in dataset_name:
         return partial(RLBenchDataPreprocessor, orig_imsize=256)
-    if 'rlbench' in dataset_name or 'peract' in dataset_name:
+    if 'peract' in dataset_name:
+        return partial(PeractDataPreprocessor, orig_imsize=256)
+    if 'rlbench' in dataset_name:
         return partial(RLBenchDataPreprocessor, orig_imsize=128)
     if 'calvin' in dataset_name:
         return partial(CALVINDataPreprocessor, orig_imsize=200)
--- utils/depth2cloud/__init__.py
+++ utils/depth2cloud/__init__.py
@@ -4,11 +4,9 @@ from .rlbench import RLBenchDepth2Cloud
 
 def fetch_depth2cloud(dataset_name):
     dataset_name = dataset_name.lower()
-    if 'mobaloha' in dataset_name:
-        return RLBenchDepth2Cloud((256, 256))
     if 'peract2' in dataset_name:
         return RLBenchDepth2Cloud((256, 256))
-    if 'rlbench' in dataset_name or 'peract' in dataset_name:
+    if 'rlbench' in dataset_name:
         return RLBenchDepth2Cloud((128, 128))
     if 'calvin' in dataset_name:
         return CALVINDepth2Cloud((200, 200))
